8bb1cd36bcbbaf25f4b4c77cc3730cdda7a91ec5
Shivam Yadav <shivamkumar12ed@gmail.com>
2026-05-12 14:57:11 +0530

    Update sum.c

--- Basics/sum.c
+++ Basics/sum.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 
 int main() {
-    printf("Sum program");
+    int a, b;
+    printf("Enter Two Numbers");
+    scanf("%d %d", &a ,&b);
+    int sum = a+b;
+    printf("Sum is %d",sum);
     return 0;
     }
